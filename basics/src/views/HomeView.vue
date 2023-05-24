1fc90b1f47b9c17be8640a9134067a32cace287a
Rafiu Moshood <meezyloaded@gmail.com>
2023-05-24 14:57:05 +0300

    use reactive data and reactive objects

--- basics/src/views/HomeView.vue
+++ basics/src/views/HomeView.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>Home</h1>
+  <div class="home">
+    <h3>{{ counterData.title }}:</h3>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input type="text" v-model="counterData.title" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { reactive } from "vue";
+
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
+
+const decreaseCounter = () => {
+  counterData.count--;
+};
+
+const increaseCounter = () => {
+  counterData.count++;
+};
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit {
+  margin-top: 60px;
+}
+</style>
